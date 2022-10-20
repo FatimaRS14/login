eca02258f571f2af8335545750b422f3d7d0d6e8
Fatty Rojas <faty.aome.rojas14@gmail.com>
2022-10-20 11:58:17 -0500

    Ontencion de datos

---
+++ js/index.js
@@ -0,0 +1,5 @@
+//declarion de variables para vincular los objetos del formulario
+const usuario = document.getElementById('user');
+const password = document.getElementById('pass');
+const formulario = document.getElementById('formLogin');
+
