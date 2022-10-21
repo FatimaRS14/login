d9ae2cf85c7d6c0694169f590a36e9a7c056b271
Fatima <faty.aome.rojas14@gmail.com>
2022-10-20 21:18:43 -0500

    Obtencion de datos de registro

--- js/index.js
+++ js/index.js
@@ -3,3 +3,26 @@ const usuario = document.getElementById('user');
 const password = document.getElementById('pass');
 const formulario = document.getElementById('formLogin');
 
+//generando eventos
+formulario.addEventListener('submit', login);
+
+//funciones a ejecutar
+function login(e){
+    e.preventDefault();
+    //console.log('Se ejecuta evento de form');
+    let usuarioVal = usuario.value;
+    let passwordVal = password.value;
+
+    //validacion de usuario y contraseña
+    if(usuarioVal == ''|| passwordVal == ''){
+        return;
+    }
+    //recuperar datos
+    console.log('el valor para user es:' + usuarioVal);
+    console.log('el valor para pass es:' + passwordVal);
+
+    
+
+}
+
+
